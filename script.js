c134ab2d1734fc065d60c6b5e83c3ff045c86be8
Carmine Esposito <105643457+KuiilMcFly@users.noreply.github.com>
2022-12-28 22:44:05 +0100

    push codice

--- script.js
+++ script.js
@@ -1,40 +1,39 @@
 //Selezionare elementi
 
+//onload
+
 const select = document.querySelectorAll(".currency");
-const btn = document.getElementById("btn");
+const btn = document.getElementById("btn-converter");
 const num = document.getElementById("conversion-form");
 const result = document.getElementById("result");
 
 //chiamare API
-  
-  async function CurrencyOptions(){
-  try{
-  const response = await fetch("https://api.frankfurter.app/currencies");
-  const data = await response.json();
-  display(data);
-  }catch(error){
+
+async function currencyOptions() {
+  try {
+    const response = await fetch("https://api.frankfurter.app/currencies");
+    const data = await response.json();
+    display(data);
+  } catch (error) {
     console.log(error);
   }
-  };
+}
 
-  window.onload=function(){
-  CurrencyOptions();
-  }
+window.onload = function () {
+  currencyOptions();
+};
 
-  
+//Mostrare opzioni valute selezionabili dall'API
+const countries1 = select[0];
+const countries2 = select[1];
 
-
-
-  //Mostrare opzioni valute selezionabili dall'API
-  function display(data) {
+function display(data) {
   const entries = data;
-
   for (const entry in entries) {
     console.log(entry);
-    select[0].innerHTML += `<option value="${entry}">${entry}</option>`;
-    select[1].innerHTML += `<option value="${entry}">${entry}</option>`;
+    countries1.innerHTML += `<option value="${entry}">${entry}</option>`;
+    countries2.innerHTML += `<option value="${entry}">${entry}</option>`;
   }
-
 }
 
 //Aggiungere Event on click quando viene cliccato tasto converti
@@ -56,21 +55,9 @@ async function convert(currency1, currency2, value) {
   await fetch(
     `https://${host}/latest?amount=${value}&from=${currency1}&to=${currency2}`
   )
-    .then((val) =>  val.json())
+    .then((val) => val.json())
     .then((val) => {
       console.log(Object.values(val.rates)[0]);
       result.value = Object.values(val.rates)[0];
     });
 }
-
-
-
-
-
-
-
-
-
-
-
-
