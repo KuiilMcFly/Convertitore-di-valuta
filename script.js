94f8738f673b59374219c077fdc843132b38b492
Carmine Esposito <105643457+KuiilMcFly@users.noreply.github.com>
2022-12-28 21:15:25 +0100

    fixed work

--- script.js
+++ script.js
@@ -3,27 +3,34 @@
 
 const select = document.querySelectorAll(".currency");
 const btn = document.getElementById("btn");
-const num = document.getElementById("input");
-const risultato = document.getElementById("result");
+const num = document.getElementById("conversion-form");
+const result = document.getElementById("result");
 
 //chiamare API
 
-  async function opzioniValute(){
+  async function CurrencyOptions(){
+  try{
   const response = await fetch("https://api.frankfurter.app/currencies");
   const data = await response.json();
   display(data);
+  }catch(error){
+    console.log(error);
+  }
   };
 
-  opzioniValute();
+  CurrencyOptions();
 
 
   //Mostrare opzioni valute selezionabili dall'API
   function display(data) {
-  const entries = Object.entries(data);
-  for (var i = 0; i < entries.length; i++) {
-    select[0].innerHTML += `<option value="${entries[i][0]}">${entries[i][0]}</option>`;
-    select[1].innerHTML += `<option value="${entries[i][0]}">${entries[i][0]}</option>`;
+  const entries = data;
+
+  for (const entry in entries) {
+    console.log(entry);
+    select[0].innerHTML += `<option value="${entry}">${entry}</option>`;
+    select[1].innerHTML += `<option value="${entry}">${entry}</option>`;
   }
+
 }
 
 //Aggiungere Event on click quando viene cliccato tasto converti
@@ -32,7 +39,7 @@ btn.addEventListener("click", () => {
   let currency2 = select[1].value;
   let value = num.value;
 
-  if (currency1 != currency2) {
+  if (currency1 !== currency2) {
     convert(currency1, currency2, value);
   } else {
     alert("Scegli una valuta diversa");
@@ -40,35 +47,17 @@ btn.addEventListener("click", () => {
 });
 
 //Definire il tasto converti
-function convert(currency1, currency2, value) {
+async function convert(currency1, currency2, value) {
   const host = "api.frankfurter.app";
-  fetch(
+  await fetch(
     `https://${host}/latest?amount=${value}&from=${currency1}&to=${currency2}`
   )
     .then((val) =>  val.json())
     .then((val) => {
       console.log(Object.values(val.rates)[0]);
-      risultato.value = Object.values(val.rates)[0];
+      result.value = Object.values(val.rates)[0];
     });
 }
-
-//Modificare il font con Javascript
-function mediaFont(font) {
-  if (font.matches) {
-    document.querySelector('h1').style.fontSize = "28px";
-  }
-}
-
-let font = window.matchMedia("(max-width: 1330px)");
-
-mediaFont(font);
-
-font.addEventListener(mediaFont);
-
-//Questo metodo per cambiare il font in base alla media query funziona
-//con il refresh della pagina, quindi la pagina deve caricare già
-//con quella risoluzione, muovendo l'inspector non cambierà nulla
-//senza il refresh della pagina
 
 
 
